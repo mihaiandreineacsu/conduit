764a06b45f526c6d8339ac16afa2a8b75e8471a6
mihaiandreineacsu <mihai@developerakademie.com>
2025-04-10 11:22:01 +0200

    project-deploy: change eof to LF

--- backend-entrypoint.sh
+++ backend-entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 (
     python manage.py makemigrations &&
-    python manage.py migrate &&
-    python manage.py runserver 0.0.0.0:8000
+        python manage.py migrate &&
+        python manage.py runserver 0.0.0.0:8000
 ) 2>&1 | tee -a /var/log/container_logs/container.log
