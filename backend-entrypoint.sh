36f95b602b6b3c4895d58ae6d90356e3f08fd90d
mihaiandreineacsu <mihai@developerakademie.com>
2025-06-06 17:10:38 +0200

    build: backend entrypoint now creates an admin if Django environments are set and runs the application in development if DJANGO_DEBUG is se to True otherwise runs it in gunicorn

--- backend-entrypoint.sh
+++ backend-entrypoint.sh
@@ -1,8 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/sh
 set -e
 
-(
-    python manage.py makemigrations &&
-        python manage.py migrate &&
-        python manage.py runserver 0.0.0.0:8000
-) 2>&1 | tee -a /var/log/container_logs/container.log
+python manage.py makemigrations
+python manage.py migrate
+
+if [ "$DJANGO_SUPERUSER_EMAIL" ] && [ "$DJANGO_SUPERUSER_USERNAME" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ]; then
+    python manage.py createsuperuser --no-input || {
+        echo "Admin was not created!"
+    }
+fi
+
+if [ "${DJANGO_DEBUG}" = "True" ]; then
+    exec python manage.py runserver 0.0.0.0:8000
+else
+    python manage.py collectstatics --no-input
+    exec gunicorn conduit.wsgi:application --bind 0.0.0.0:8000
+fi
