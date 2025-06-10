ef76c52303222d51f29b1cfdcb1721f5eaa316fc
mihaiandreineacsu <mihai@developerakademie.com>
2025-06-10 12:36:55 +0200

    fix: correct django command typo

--- backend-entrypoint.sh
+++ backend-entrypoint.sh
@@ -13,6 +13,6 @@ fi
 if [ "${DJANGO_DEBUG}" = "True" ]; then
     exec python manage.py runserver 0.0.0.0:8000
 else
-    python manage.py collectstatics --no-input
+    python manage.py collectstatic --no-input
     exec gunicorn conduit.wsgi:application --bind 0.0.0.0:8000
 fi
