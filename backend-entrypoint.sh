ae199d34c0320ae3d729fb7dbf9e6c97afcfc3cf
mihaiandreineacsu <mihai@developerakademie.com>
2025-07-03 17:37:06 +0200

    chore: remove makemigrations, migrate and createsuperuser django commands calls from backend container entrypoint

--- backend-entrypoint.sh
+++ backend-entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 set -e
 
-python manage.py makemigrations
-python manage.py migrate
-
-if [ "$DJANGO_SUPERUSER_EMAIL" ] && [ "$DJANGO_SUPERUSER_USERNAME" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ]; then
-    python manage.py createsuperuser --no-input || {
-        echo "Admin was not created!"
-    }
-fi
-
 if [ "${DJANGO_DEBUG}" = "True" ]; then
     exec python manage.py runserver 0.0.0.0:8000
 else
